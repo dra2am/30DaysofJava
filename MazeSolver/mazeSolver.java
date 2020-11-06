d3ff5279fda7e79c3c7d5d8c2daee13bc0a059c1
Diana A <dralcantara96@gmail.com>
2020-11-05 22:17:54 -0500

    added ability to solve multiple mazes

--- MazeSolver/mazeSolver.java
+++ MazeSolver/mazeSolver.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class mazeSolver {
@@ -16,7 +15,55 @@ public class mazeSolver {
 
     public static void main(String[] args) throws FileNotFoundException {
         // list of mazes
-        ArrayList<Maze> mazes = new ArrayList<Maze>();
+        ArrayList<Maze> mazes = readMazes();
+
+        // get maze from file
+        Scanner in = new Scanner(new File("mazeSolver/mazes.txt"));
+
+        while (in.hasNext()) {
+            Maze m = new Maze();
+
+            // get number of rows
+            int rows = Integer.parseInt(in.nextLine());
+
+            m.maze = new int[rows][];
+
+            // loops through file
+            for (int i = 0; i < rows; i++) {
+                // prints first next line as string
+                String line = in.nextLine();
+                // convert string to int arr
+                m.maze[i] = Arrays.stream(line.split(", ")).mapToInt(Integer::parseInt).toArray();
+            }
+
+            // get starting position
+            m.start = new Position(Integer.parseInt(in.nextLine()), Integer.parseInt(in.nextLine()));
+            // toss extra space
+            in.nextLine();
+            // and add
+            mazes.add(m);
+        }
+
+        // close scanner
+        in.close();
+
+        // loops through arraylist maze to solve all mazes
+        int i = 0;
+        while (i < mazes.size()) {
+            // loop through arraylist
+            // calling code is in charge of success or fail
+            if (solveMaze(mazes.get(i))) {
+                System.out.println("You won!");
+            } else {
+                System.out.println("No path ):");
+            }
+            i++;
+        }
+
+    }
+
+    private static ArrayList<Maze> readMazes() throws FileNotFoundException {
+        ArrayList<Maze> mazes = new ArrayList<>();
         Maze m = new Maze();
 
         // get maze from file
@@ -37,19 +84,7 @@ public class mazeSolver {
         m.start = new Position(Integer.parseInt(in.nextLine()), Integer.parseInt(in.nextLine()));
         mazes.add(m);
 
-        // loops through arraylist maze to solve all mazes
-        int i = 0;
-        while (i < mazes.size()) {
-            // loop through arraylist
-            // calling code is in charge of success or fail
-            if (solveMaze(mazes.get(i))) {
-                System.out.println("You won!");
-            } else {
-                System.out.println("No path ):");
-            }
-            i++;
-        }
-
+        return mazes;
     }
 
     private static boolean solveMaze(Maze m) {
